2826c55b659beeb4dabb4fd8d3b48caec8fad26f
Max Cao <macao@redhat.com>
2025-03-05 13:04:11 -0800

    test
    Signed-off-by: Max Cao <macao@redhat.com>

--- test/suites/perf/scheduling_test.go
+++ test/suites/perf/scheduling_test.go
@@ -22,7 +22,6 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/samber/lo"
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,7 +32,7 @@ import (
 )
 
 var _ = Describe("Performance", func() {
-	var replicas = 100
+	var replicas = 2
 
 	Context("Provisioning", func() {
 		It("should do simple provisioning", func() {
@@ -89,68 +88,68 @@ var _ = Describe("Performance", func() {
 			}).WithTimeout(10 * time.Minute).Should(Succeed())
 			env.TimeIntervalCollector.End("Drift")
 		})
-		It("should do complex provisioning", func() {
-			deployments := []*appsv1.Deployment{}
-			podOptions := test.MakeDiversePodOptions()
-			totalReplicas := 0
-			for _, option := range podOptions {
-				podDensity := replicas / len(podOptions)
-				totalReplicas += podDensity
-				deployments = append(deployments, test.Deployment(
-					test.DeploymentOptions{
-						PodOptions: option,
-						Replicas:   int32(podDensity),
-					},
-				))
-			}
-			for _, dep := range deployments {
-				env.ExpectCreated(dep)
-			}
-			env.TimeIntervalCollector.Start("PostDeployment")
-			env.ExpectCreated(nodePool, nodeClass)
-			env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, totalReplicas)
-			env.TimeIntervalCollector.End("PostDeployment")
-		})
-		It("should do complex provisioning and complex drift", func() {
-			deployments := []*appsv1.Deployment{}
-			podOptions := test.MakeDiversePodOptions()
-			totalReplicas := 0
-			for _, option := range podOptions {
-				podDensity := replicas / len(podOptions)
-				totalReplicas += podDensity
-				deployments = append(deployments, test.Deployment(
-					test.DeploymentOptions{
-						PodOptions: option,
-						Replicas:   int32(podDensity),
-					},
-				))
-			}
-			for _, dep := range deployments {
-				env.ExpectCreated(dep)
-			}
+		// It("should do complex provisioning", func() {
+		// 	deployments := []*appsv1.Deployment{}
+		// 	podOptions := test.MakeDiversePodOptions()
+		// 	totalReplicas := 0
+		// 	for _, option := range podOptions {
+		// 		podDensity := replicas / len(podOptions)
+		// 		totalReplicas += podDensity
+		// 		deployments = append(deployments, test.Deployment(
+		// 			test.DeploymentOptions{
+		// 				PodOptions: option,
+		// 				Replicas:   int32(podDensity),
+		// 			},
+		// 		))
+		// 	}
+		// 	for _, dep := range deployments {
+		// 		env.ExpectCreated(dep)
+		// 	}
+		// 	env.TimeIntervalCollector.Start("PostDeployment")
+		// 	env.ExpectCreated(nodePool, nodeClass)
+		// 	env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, totalReplicas)
+		// 	env.TimeIntervalCollector.End("PostDeployment")
+		// })
+		// It("should do complex provisioning and complex drift", func() {
+		// 	deployments := []*appsv1.Deployment{}
+		// 	podOptions := test.MakeDiversePodOptions()
+		// 	totalReplicas := 0
+		// 	for _, option := range podOptions {
+		// 		podDensity := replicas / len(podOptions)
+		// 		totalReplicas += podDensity
+		// 		deployments = append(deployments, test.Deployment(
+		// 			test.DeploymentOptions{
+		// 				PodOptions: option,
+		// 				Replicas:   int32(podDensity),
+		// 			},
+		// 		))
+		// 	}
+		// 	for _, dep := range deployments {
+		// 		env.ExpectCreated(dep)
+		// 	}
 
-			env.ExpectCreated(nodePool, nodeClass)
-			env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, totalReplicas)
+		// 	env.ExpectCreated(nodePool, nodeClass)
+		// 	env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, totalReplicas)
 
-			env.TimeIntervalCollector.Start("Drift")
-			nodePool.Spec.Template.ObjectMeta.Labels = lo.Assign(nodePool.Spec.Template.ObjectMeta.Labels, map[string]string{
-				"test-drift": "true",
-			})
-			env.ExpectUpdated(nodePool)
-			// Eventually expect one node to be drifted
-			Eventually(func(g Gomega) {
-				nodeClaims := &v1.NodeClaimList{}
-				g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1.ConditionTypeDrifted})).To(Succeed())
-				g.Expect(len(nodeClaims.Items)).ToNot(Equal(0))
-			}).WithTimeout(5 * time.Second).Should(Succeed())
-			// Then eventually expect no nodes to be drifted
-			Eventually(func(g Gomega) {
-				nodeClaims := &v1.NodeClaimList{}
-				g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1.ConditionTypeDrifted})).To(Succeed())
-				g.Expect(len(nodeClaims.Items)).To(Equal(0))
-			}).WithTimeout(10 * time.Minute).Should(Succeed())
-			env.TimeIntervalCollector.End("Drift")
-		})
+		// 	env.TimeIntervalCollector.Start("Drift")
+		// 	nodePool.Spec.Template.ObjectMeta.Labels = lo.Assign(nodePool.Spec.Template.ObjectMeta.Labels, map[string]string{
+		// 		"test-drift": "true",
+		// 	})
+		// 	env.ExpectUpdated(nodePool)
+		// 	// Eventually expect one node to be drifted
+		// 	Eventually(func(g Gomega) {
+		// 		nodeClaims := &v1.NodeClaimList{}
+		// 		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1.ConditionTypeDrifted})).To(Succeed())
+		// 		g.Expect(len(nodeClaims.Items)).ToNot(Equal(0))
+		// 	}).WithTimeout(5 * time.Second).Should(Succeed())
+		// 	// Then eventually expect no nodes to be drifted
+		// 	Eventually(func(g Gomega) {
+		// 		nodeClaims := &v1.NodeClaimList{}
+		// 		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1.ConditionTypeDrifted})).To(Succeed())
+		// 		g.Expect(len(nodeClaims.Items)).To(Equal(0))
+		// 	}).WithTimeout(10 * time.Minute).Should(Succeed())
+		// 	env.TimeIntervalCollector.End("Drift")
+		// })
 	})
 
 })
